9cf0193bcbed3937782f50774d8b2933417377b5
VictorEmanuel08 <victorem3181@gmail.com>
2023-02-13 11:20:23 -0300

    Att 13/02

--- main.js
+++ main.js
@@ -18,3 +18,18 @@ for (const link of links) {
     nav.classList.remove("show");
   });
 }
+
+// mudar o header da página quando der scroll
+
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHeight) {
+    // maior que a altura do header
+    header.classList.add("scroll");
+  } else {
+    // menor que a altura do header
+    header.classList.remove("scroll");
+  }
+});
